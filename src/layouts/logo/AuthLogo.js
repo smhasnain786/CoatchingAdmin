7b98c466156b77ff73917e951705c9f2a3188245
syed <syedhasnain7456@gmail.com>
2024-10-13 17:38:21 +0530

    first commit

--- src/layouts/logo/AuthLogo.js
+++ src/layouts/logo/AuthLogo.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 
-import { ReactComponent as LogoDarkIcon } from '../../assets/images/logos/elite-dark-icon.svg';
 import LogoDarkText from '../../assets/images/logos/logo-text.png';
-import { ReactComponent as LogoWhiteIcon } from '../../assets/images/logos/elite-white-icon.svg';
 import LogoWhiteText from '../../assets/images/logos/logo-light-text.png';
 
 const AuthLogo = () => {
