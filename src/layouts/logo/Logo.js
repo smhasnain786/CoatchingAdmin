a2ecde55c6d0e46c21963823ca6ae789d077d751
syed <syedhasnain7456@gmail.com>
2024-10-13 18:04:15 +0530

    first commit

--- src/layouts/logo/Logo.js
+++ src/layouts/logo/Logo.js
@@ -1,9 +1,7 @@
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import React from 'react';
-import { ReactComponent as LogoDarkIcon } from '../../assets/images/logos/elite-dark-icon.svg';
 import LogoDarkText from '../../assets/images/logos/logo-text.png';
-import { ReactComponent as LogoWhiteIcon } from '../../assets/images/logos/elite-white-icon.svg';
 import LogoWhiteText  from '../../assets/images/logos/logo-light-text.png';
 
 const Logo = () => {
